a3572120b718e7f1246b9bc1f0662f53b4f93e6e
EtumianAI <115877747+EtumianAI@users.noreply.github.com>
2026-07-23 20:50:18 +0300

    создан новый проект

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Search from "./components/Search";
+import WeatherCard from "./components/WeatherCard";
+
+function App() {
+  return (
+    <div>
+      <h1>Прогноз погоды</h1>
+      <Search />
+      <WeatherCard />
+    </div>
+  );
+}
---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,7 @@
+export default function WeatherCard() {
+  return (
+    <div>
+      <h2>🌤️ Здесь будет погода</h2>
+    </div>
+  );
+}
